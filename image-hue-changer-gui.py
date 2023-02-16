d65b34a50f6cb2195bc3ce0847d4fa5cf61d654c
buergerr <contact@robertbuerger.de>
2023-02-16 22:27:09 +0100

    added settings.json to store last settings

--- image-hue-changer-gui.py
+++ image-hue-changer-gui.py
@@ -3,6 +3,9 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import subprocess
 from tkinter import colorchooser
+import json
+
+SETTINGS_FILE = 'settings.json'
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -27,6 +30,7 @@ class Application(tk.Frame):
         self.output_folder_entry.grid(row=1, column=1, padx=10, pady=10, sticky='w')
         self.output_folder_button = tk.Button(self.master, text='Browse', command=self.select_output_folder)
         self.output_folder_button.grid(row=1, column=2, padx=10, pady=10, sticky='w')
+        self.output_folder = 'C:/output' # Set the default output folder here
 
         self.color_label = tk.Label(self.master, text='Select Color:')
         self.color_label.grid(row=2, column=0, padx=10, pady=10, sticky='w')
@@ -38,12 +42,55 @@ class Application(tk.Frame):
         self.process_button = tk.Button(self.master, text='Process Images', command=self.process_images)
         self.process_button.grid(row=3, column=1, padx=10, pady=10)
 
+        self.load_settings()
+        
+
+    def load_settings(self):
+        self.input_folder = ''
+        self.output_folder = ''
+        self.color = ''
+        if os.path.exists(SETTINGS_FILE):
+            with open(SETTINGS_FILE, 'r') as f:
+                settings = json.load(f)
+                self.input_folder = settings['input_folder']
+                self.output_folder = settings['output_folder']
+                self.color = settings['color']
+
+        if self.input_folder:
+            self.input_folder_entry.configure(state='normal')
+            self.input_folder_entry.delete(0, tk.END)
+            self.input_folder_entry.insert(0, self.input_folder)
+            self.input_folder_entry.configure(state='readonly')
+
+        if self.output_folder:
+            self.output_folder_entry.configure(state='normal')
+            self.output_folder_entry.delete(0, tk.END)
+            self.output_folder_entry.insert(0, self.output_folder)
+            self.output_folder_entry.configure(state='readonly')
+
+        if self.color:
+            self.color_entry.configure(state='normal')
+            self.color_entry.delete(0, tk.END)
+            self.color_entry.insert(0, self.color)
+            self.color_entry.configure(state='readonly')
+
+    def save_settings(self):
+        settings = {
+            'input_folder': self.input_folder,
+            'output_folder': self.output_folder,
+            'color': self.color
+        }
+        with open(SETTINGS_FILE, 'w') as f:
+            json.dump(settings, f)
+      
+
     def select_input_folder(self):
         self.input_folder = filedialog.askdirectory()
         self.input_folder_entry.configure(state='normal')
         self.input_folder_entry.delete(0, tk.END)
         self.input_folder_entry.insert(0, self.input_folder)
         self.input_folder_entry.configure(state='readonly')
+        self.save_settings()
 
     def select_output_folder(self):
         self.output_folder = filedialog.askdirectory()
@@ -51,6 +98,7 @@ class Application(tk.Frame):
         self.output_folder_entry.delete(0, tk.END)
         self.output_folder_entry.insert(0, self.output_folder)
         self.output_folder_entry.configure(state='readonly')
+        self.save_settings()
 
     def pick_color(self):
         self.color = colorchooser.askcolor()[1]
@@ -59,8 +107,9 @@ class Application(tk.Frame):
             self.color_entry.delete(0, tk.END)
             self.color_entry.insert(0, self.color)
             self.color_entry.configure(state='readonly')
+            self.save_settings()
 
-
+    
     def process_images(self):
         if not self.input_folder or not self.output_folder or not self.color:
             messagebox.showerror('Error', 'Please select input folder, output folder, and color')
@@ -72,9 +121,12 @@ class Application(tk.Frame):
                 output_file = os.path.join(self.output_folder, filename)
                 subprocess.call(['magick', 'convert', input_file, '-modulate', '100,0,100', '-fill', self.color, '-tint', '100', output_file])
         messagebox.showinfo('Information', 'Images processed successfully')
+        self.save_settings()
+        
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.title('Image Huer')
+    root.title('Batch Image Huer')
     app = Application(master=root)
     app.mainloop()
+    
